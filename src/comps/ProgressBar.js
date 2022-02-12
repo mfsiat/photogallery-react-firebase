fcfe2b6c40c3eaf92499b30717f90efba56e96fe
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-02-13 00:54:15 +0600

    commented out progress bar

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,22 +1,22 @@
-import { motion } from 'framer-motion';
-import React, { useEffect } from 'react';
-import useStorage from '../hooks/useStorage';
+// import { motion } from 'framer-motion';
+// import React, { useEffect } from 'react';
+// import useStorage from '../hooks/useStorage';
 
-const ProgressBar = ({ file, setFile }) => {
-  const { progress, url } = useStorage(file);
+// const ProgressBar = ({ file, setFile }) => {
+//   const { progress, url } = useStorage(file);
 
-  useEffect(() => {
-    if (url) {
-      setFile(null);
-    }
-  }, [url, setFile]);
+//   useEffect(() => {
+//     if (url) {
+//       setFile(null);
+//     }
+//   }, [url, setFile]);
 
-  return (
-    <motion.div className="progress-bar"
-      initial={{ width: 0 }}
-      animate={{ width: progress + '%' }}
-    ></motion.div>
-  );
-} 
+//   return (
+//     <motion.div className="progress-bar"
+//       initial={{ width: 0 }}
+//       animate={{ width: progress + '%' }}
+//     ></motion.div>
+//   );
+// } 
 
-export default ProgressBar;
+// export default ProgressBar;
