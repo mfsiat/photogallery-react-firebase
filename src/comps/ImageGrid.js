57ffc9ad7374182d8b7345d2e0b3eba23bf8d503
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-02-22 02:52:07 +0600

    configured framer motion

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
 
@@ -8,9 +9,20 @@ const ImageGrid = ({setSelectedImg}) => {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div className="img-wrap" key={doc.id} onClick={() => setSelectedImg(doc.url)}>
-            <img src={doc.url} alt="uploaded picture" />
-          </div>
+          <motion.div 
+            className="img-wrap" 
+            key={doc.id} 
+            layout
+            whileHover={{ opacity: 1 }}
+            onClick={() => setSelectedImg(doc.url)}>
+              <motion.img 
+                src={doc.url} 
+                alt="uploaded picture"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1 }}
+              />
+          </motion.div>
         ))}
     </div>
   );
--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
@@ -8,9 +9,15 @@ const Modal = ({ selectedImg, setSelectedImg }) => {
   };
 
   return (
-    <div className="backdrop" onClick={handleClick}>
-      <img src={selectedImg} alt="enlarged pic" />
-    </div>
+    <motion.div className="backdrop" onClick={handleClick}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+    >
+      <motion.img src={selectedImg} alt="enlarged pic" 
+        initial={{ y:"-100vh" }}
+        animate={{ y: 0 }}
+      />
+    </motion.div>
   );
 };
 
