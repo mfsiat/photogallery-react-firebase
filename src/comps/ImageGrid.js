b33184feda871ef486bd24140435899f8d8a2094
Md Nasirul Islam <nasirul.siat@outlook.com>
2022-02-22 01:27:45 +0600

    view image on front

---
+++ src/comps/ImageGrid.js
@@ -0,0 +1,19 @@
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
+
+const ImageGrid = () => {
+  const { docs } = useFirestore("images");
+  console.log(docs);
+  return (
+    <div className="App">
+      {docs &&
+        docs.map((doc) => (
+          <div className="img-wrap" key={doc.id}>
+            <img src={doc.url} alt="uploaded picture" />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ImageGrid;
